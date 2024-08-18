038e616fd59de609643fe375df9a3c11abdf8fa3
racwhi <128566218+racwhi@users.noreply.github.com>
2024-08-18 13:21:15 -1000

    week 1 complete

--- week-1/chapter/js01b.js
+++ week-1/chapter/js01b.js
@@ -3,8 +3,8 @@
     Chapter case
 
     Tinley Xeriscapes
-    Author: 
-    Date:   
+    Author: Rachel White
+    Date: 08/18/2024
 
     Filename: js01b.js
 */
@@ -15,24 +15,25 @@ let rugosa = "rugosa.jpg";
 let bluestem = "bluestem.jpg";
 
 //add event handlers to the list items
-document.getElementById("blanket").onclick = function() {
-   document.getElementById("plantImg").src = blanket;
-}
-document.getElementById("rugosa").onclick = function() {
-   document.getElementById("plantImg").src = rugosa;
-}
-document.getElementById("bluestem").onclick = function() {
-   document.getElementById("plantImg").src = bluestem;
-}
+document.getElementById("blanket").onclick = function () {
+  document.getElementById("plantImg").src = blanket;
+};
+document.getElementById("rugosa").onclick = function () {
+  document.getElementById("plantImg").src = rugosa;
+};
+document.getElementById("bluestem").onclick = function () {
+  document.getElementById("plantImg").src = bluestem;
+};
 
 /*
    Information on available plants
    including link to USDA website
 */
 
-let captionText = "<p>Plant choices for " +
-                  "<a href='http://planthardiness.ars.usda.gov'>" +
-                  "hardiness zones</a>" +
-                  " 5a - 6b </p>";
+let captionText =
+  "<p>Plant choices for " +
+  "<a href='http://planthardiness.ars.usda.gov'>" +
+  "hardiness zones</a>" +
+  " 5a - 6b </p>";
 
 document.getElementById("imgCaption").innerHTML = captionText;
--- week-1/project01-02/project01-02_txt.js
+++ week-1/project01-02/project01-02_txt.js
@@ -2,10 +2,19 @@
     Chapter 1
     Hands-On Project 1-2
 
-    Author: 
-    Date:   
+    Author: Rachel White
+    Date:  08/18/2024
 
     Filename: project01-02.js
 */
 
 //define variables for service name and service speed
+
+let service1Name = "Basic",
+  service2Name = "Express",
+  service3Name = "Extreme",
+  service4Name = "Ultimate";
+let service1Speed = "0 Mbps",
+  service2Speed = "100 Mbps",
+  service3Speed = "500 Mbps",
+  service4Speed = "1 Gig";
