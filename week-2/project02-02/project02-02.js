f3b9caefea5cbb1f99fc7c455cd7a3e986bf0d08
racwhi <128566218+racwhi@users.noreply.github.com>
2024-08-25 15:28:00 -1000

    week2 changes

---
+++ week-2/project02-02/project02-02.js
@@ -0,0 +1,24 @@
+/*    JavaScript 7th Edition
+      Chapter 2
+      Project 02-02
+
+      Application to test for completed form
+      Author: 
+      Date:   
+
+      Filename: project02-02.js
+ */
+
+function verifyForm() {
+  //declare variables to equal value of input
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const phone = document.getElementById("phone").value;
+
+  //conditional operator that tests the truly or falsy value of and name and email and phone using the && operator. If true alert Thank you otherwise .
+  name && email && phone
+    ? window.alert("Thank You")
+    : window.alert("Please fill in all fields");
+}
+
+document.getElementById("submit").addEventListener("click", verifyForm);
