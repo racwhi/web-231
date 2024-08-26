93e03d9946af0e3ccdcb20d9cddbd05574d40e31
racwhi <128566218+racwhi@users.noreply.github.com>
2024-08-25 15:46:31 -1000

    update author and date

--- week-2/project02-02/project02-02.js
+++ week-2/project02-02/project02-02.js
@@ -3,8 +3,8 @@
       Project 02-02
 
       Application to test for completed form
-      Author: 
-      Date:   
+      Author: Rachel White
+      Date: 8/25/2024   
 
       Filename: project02-02.js
  */
